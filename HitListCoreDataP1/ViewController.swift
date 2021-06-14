89a8b3b035409b6d89f26d79ccc42e72b1b0a973
Alexey Barinov <begoodbebad2012@gmail.com>
2021-06-14 10:05:39 +0300

    Initial Commit

---
+++ HitListCoreDataP1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HitListCoreDataP1
+//
+//  Created by Mac on 14.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
